c55a49fa01502c8e8c0f2c2f2997b92b53e14d25
aya <ayasayedmostafa8@gmail.com>
2026-07-26 15:55:35 +0300

    try port 587 for smtp

--- src/modules/email/email.service.js
+++ src/modules/email/email.service.js
@@ -3,7 +3,10 @@ import path from 'path';
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  service: 'Gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
